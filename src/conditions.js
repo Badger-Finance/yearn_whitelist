ba4574976517a2b97f71498a2bcba1a1de0a50f9
Darruma <umerderr@gmail.com>
2021-04-07 16:46:45 +0100

    altered condition tooltips

--- src/conditions.js
+++ src/conditions.js
@@ -1,23 +1,23 @@
 const conditions = {
   cond1: {
     condition: "Badger staking and LP before Digg launch",
-    tooltip: "Badger staking on 12/01, 12/29, 1/8, 1/18",
+    tooltip: "Badger staked on 12/01, 12/29, 1/8, 1/18",
   },
   cond2: {
     condition: "Badger staking and LP after Digg launch",
-    tooltip: "Badger staking on 1/26, 2/11, 2/28, 3/17",
+    tooltip: "Badger staked on 1/26, 2/11, 2/28, 3/17",
   },
   cond3: {
     condition: "In a non-native Sett since Badger Launch",
-    tooltip: "Sett balance > 0 on 12/10, 1/8, 2/11, 3/17",
+    tooltip: "Non native sett balance > 0 on 12/10, 1/8, 2/11, 3/17",
   },
   cond4: {
     condition: "Digg staker and LP after Digg Launch",
-    tooltip: "Digg staking > 0 on 1/26, 2/11, 2/28, 3/17",
+    tooltip: "Digg staked/LP > 0 on 1/26, 2/11, 2/28, 3/17",
   },
   cond5: {
     condition: "Digg supporter below peg",
-    tooltip: "Balance higher on 3/17 than 2/18",
+    tooltip: "Digg balance higher on 3/17 than 2/18",
   },
   cond6: {
     condition: "Badger governance participant",
