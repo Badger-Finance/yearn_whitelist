7c5fb51ac8a89a258296b62f75efbefab5cbf770
Umair Darr <umerderr@gmail.com>
2021-04-14 08:53:33 +0100

    add condition all text

--- src/conditions.js
+++ src/conditions.js
@@ -1,23 +1,23 @@
 const conditions = {
   cond1: {
     condition: "Badger staking and LP before Digg launch",
-    tooltip: "Badger staked/LP on 12/10, 12/29, 1/8, 1/18",
-  },
+    tooltip: "Badger staked/LP on ALL 12/10, 12/29, 1/8, 1/18",
+  }, 
   cond2: {
     condition: "Badger staking and LP after Digg launch",
-    tooltip: "Badger staked/LP on 1/26, 2/11, 2/28, 3/17",
+    tooltip: "Badger staked/LP on ALL  1/26, 2/11, 2/28, 3/17",
   },
   cond3: {
     condition: "In a non-native Sett since Badger Launch",
-    tooltip: "Non native sett balance > 0 on 12/10, 1/8, 2/11, 3/17",
+    tooltip: "Non native sett balance > 0 on ALL 12/10, 1/8, 2/11, 3/17",
   },
   cond4: {
     condition: "Digg staker and LP after Digg Launch",
-    tooltip: "Digg staked/LP > 0 on 1/26, 2/11, 2/28, 3/17",
+    tooltip: "Digg staked/LP > 0 on ALL 1/26, 2/11, 2/28, 3/17",
   },
   cond5: {
     condition: "Digg supporter below peg",
-    tooltip: "Digg balance higher on 3/17 than 2/18",
+    tooltip: "Digg balance higher on ALL 3/17 than 2/18",
   },
   cond6: {
     condition: "Badger governance participant",
