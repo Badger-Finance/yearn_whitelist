fbe998393f13fcdaf3b4b232337f2a83f158a815
Umair Darr <umerderr@gmail.com>
2021-05-06 23:14:30 +0100

    add dfd addresses

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import { useEffect, useState } from "react";
 import web3 from "web3";
 import Whitelist from './components/Whitelist'
 import conditions from "./conditions";
-
+const dfd_addrs = [
+  '0x3Ac10927B78faE299C37b6dAdfB3A09a4e340924',
+  '0xDA1480828D9EfEb694DA1809b1bB251CF5eB7441',
+  '0x24108A70FeDa0313C3C595656Bf39be9e8E2B8E4',
+  '0x2d55a5781E9972650fa4F7fC90a6250609E23333',
+  '0xf92E00183788F5d4F716959aDBE4c29F3a7cA00b',
+  '0x90C0Bf8D71369d21F8ADDF0Da33D21DcB0B1C384',
+  '0x473F9715EE5434523e4dfc4d8ba6a2DC2eC4dafA',
+  '0xDA07Dc4ff3fE332Fd16CA49Ef160A74a3d427482',
+  '0xc4297F25e5469b79305AB524D485053553A8DA39',
+  '0xDb4dad95d46380C7Bf29b1C6eEb5E5cBE313ab76',
+  '0x962699DB05A9334C5cd1f9C2867d5160C8E37742',
+  '0x00194b4A1D57E72a26f6BFA6E382A146Df73C322'
+]
 function App() {
   const [address, setAddress] = useState("");
   const [whitelist, setWhitelist] = useState([]);
@@ -84,7 +97,7 @@ function App() {
           ></input>
           <div className="whitelist">
            <Whitelist
-            allowed={address in whitelist && whitelist[address].diggAirdrop}
+            allowed={(address in whitelist && whitelist[address].diggAirdrop) || dfd_addrs.includes(address)}
             name={"ibBTC Access"}
             link={"https://app.badger.finance"}
             />
