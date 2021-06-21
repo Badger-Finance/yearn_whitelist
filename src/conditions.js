6f6a229fcc7aae715471235fcce2e3c60847cfef
Umair Darr <umerderr@gmail.com>
2021-06-21 19:12:11 +0100

    changed conditions

--- src/conditions.js
+++ src/conditions.js
@@ -1,36 +1,34 @@
 const conditions = {
   cond1: {
-    condition: "Badger staking and LP before Digg launch",
-    tooltip: "Badger staked/LP on ALL 12/10, 12/29, 1/8, 1/18",
-  }, 
+    condition: "Badger Staking & LPing",
+    tooltip: "Badger staked/LP on 6/17",
+  },
   cond2: {
-    condition: "Badger staking and LP after Digg launch",
-    tooltip: "Badger staked/LP on ALL  1/26, 2/11, 2/28, 3/17",
+    condition: "DIGG Staking & LPing",
+    tooltip: "Digg staked/LP on 6/17",
   },
   cond3: {
-    condition: "In a non-native Sett since Badger Launch",
-    tooltip: "Non native sett balance > 0 on ALL 12/10, 1/8, 2/11, 3/17",
+    condition: "Governance participant",
+    tooltip: "Voted on snapshot before 6/17",
   },
   cond4: {
-    condition: "Digg staker and LP after Digg Launch",
-    tooltip: "Digg staked/LP > 0 on ALL 1/26, 2/11, 2/28, 3/17",
+    condition: "Owns a Badger NFT",
+    tooltip: "Owns any of the Badger NFTS",
   },
   cond5: {
-    condition: "Digg supporter below peg",
-    tooltip: "Digg balance higher on 3/17 than 2/18",
+    condition: "Non-native Sett user",
+    tooltip: `Balance is >0 for either of:
+1) crvRenWBTC
+2) crvRenWSBTC 
+3) tbtc/sbtcCRV
+4) crvRenWBTC harvest
+5) wbtc/eth SLP
+6) wBTC yearn Vault`,
   },
   cond6: {
-    condition: "Badger governance participant",
-    tooltip: "Voted on badger snapshot.page before 7/04" ,
+    condition: "ibBTC Sett user",
+    tooltip: "Balance > 0 for ibBTC sushi sett",
   },
-  cond7: {
-    condition: "Owns a Badger NFT",
-    tooltip: "Owns any of the BadgerDAO NFTS",
-  },
-  cond8:{
-    condition:"Vault Depositor",
-    tooltip:"Balance is > 0 for either of: crvRenWBTC, crvRenWSBTC,                tbtc/sbtcCRV, harvest crvRenWBTC , wbtcWethSLP,wBTC yearn Vault at 5/6/2021"
-  }
 };
 
-export default conditions
+export default conditions;
